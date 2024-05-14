811a462abf71b809fac2c750d5af6ea744caf58a
ageraaniska23 <agraaniska@gmail.com>
2024-05-14 20:17:04 +0700

    update

--- fe/src/components/navbar/Sidebar.jsx
+++ fe/src/components/navbar/Sidebar.jsx
@@ -79,7 +79,7 @@ const Sidebar = () => {
             </li>
             <li>
               <a
-                href="/E"
+                href="/"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
@@ -132,7 +132,7 @@ const Sidebar = () => {
             </li>
             <li>
               <a
-                href="/Employee"
+                href="/"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
